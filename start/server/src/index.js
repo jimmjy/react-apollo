4589c8df510696a250cc369891bcb88250e37626
James Finkelstein <jfinkelstein9@me.com>
2020-03-11 19:59:48 -0400

    setup data sources for api and sql data

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,10 +1,20 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
 
 const server = new ApolloServer({
-	typeDefs,
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
 });
 
 server.listen().then(({ url }) => {
-	console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,59 +1,59 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-	# Your schema will go here
-	type Launch {
-		id: ID!
-		site: String
-		mission: Mission
-		rocket: Rocket
-		isBooked: Boolean!
-	}
+  # Your schema will go here
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
 
-	type Rocket {
-		id: ID!
-		name: String
-		type: String
-	}
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
 
-	type User {
-		id: ID!
-		email: String!
-		trips: [Launch]!
-	}
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
 
-	type Mission {
-		name: String
-		missionPatch(size: PatchSize): String
-	}
+  type Mission {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
 
-	# Type def for size
-	enum PatchSize {
-		SMALL
-		LARGE
-	}
+  # Type def for size
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
 
-	type Query {
-		# returns array
-		launches: [Launch]!
-		# returns type launch
-		launch(id: ID!): Launch
-		# returns a user that is logged in
-		me: User
-	}
+  type Query {
+    # returns array
+    launches: [Launch]!
+    # returns type launch
+    launch(id: ID!): Launch
+    # returns a user that is logged in
+    me: User
+  }
 
-	type Mutation {
-		# the return types are brand new, make sure to remember ********
-		bookTrips(launchIds: [ID]!): TripUpdateResponse!
-		cancelTrip(launchID: ID!): TripUpdateResponse!
-		login(email: String): String # login token
-	}
+  type Mutation {
+    # the return types are brand new, make sure to remember ********
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchID: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
 
-	type TripUpdateResponse {
-		success: Boolean!
-		message: String
-		launches: [Launch]
-	}
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
 `;
 
 module.exports = typeDefs;
